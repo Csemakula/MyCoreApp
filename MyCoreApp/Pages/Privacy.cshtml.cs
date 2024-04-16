175f3142ea0c46a87ad5955ae35e5c91b85953ac
Ssemakula Mukiibi <ssemakula.mukiibi@chromepartners.co.ke>
2024-04-16 12:32:57 +0300

    Add project files.

---
+++ MyCoreApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MyCoreApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Datetime dd/M/yyyy H:mm:ss
+            string dateTime = DateTime.Now.ToString("dd/M/yyyy H:mm:ss", new CultureInfo("en-UK"));
+            string myCopy = "This is done by Ssemakula";
+            ViewData["TimeStamp"] = dateTime;
+            ViewData["MyCopy"] = myCopy;
+        }
+    }
+
+}
